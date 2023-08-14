29bf48483b1b26af2c02a4623b6671be8c8e71ab
krisine <kristine.nyaga@student.moringaschool.com>
2023-08-14 13:56:04 +0300

    server logic

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask
+from flask import Flask,render_template
+from flask.json import jsonify
 
-app=Flask(__name__)
-
+app = Flask(__name__, static_folder='static')
+JOBS=[
+    {
+        'id':1,
+        'title':'Data Analyst',
+        'location':'Delhi,India',
+        'salary':'Rs. 15,000'
+    },
+        {
+        'id':1,
+        'title':'Data Analyst',
+        'location':'Delhi,India',
+        'salary':'Rs. 15,000'
+    },
+        {
+        'id':1,
+        'title':'front end developer',
+        'location':'Delhi,India',
+        'salary':'Rs. 18,000'
+    },
+    {
+        'id': 1,
+        'title': 'Data scientist',
+        'location': 'Delhi,India',
+        'salary': 'Rs. 17,000'
+    }
+]
 @app.route('/')
 def home():
-    return '<h1>hello</h1>'
-
+    return render_template('home.html',jobs=JOBS)
+@app.route('/api/jobs')
+def jobs():
+    return jsonify(JOBS)
 
 if __name__ == '__main__':
     app.run(debug=True)
