406c73639d362cb93c9c1af778b0d624481c9ea4
krisine <kristine.nyaga@student.moringaschool.com>
2023-08-17 23:23:24 +0300

    server logic

--- app.py
+++ app.py
@@ -1,39 +1,34 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from flask.json import jsonify
-
+from database import load_jobs,load_job,apply_job
 app = Flask(__name__, static_folder='static')
-JOBS=[
-    {
-        'id':1,
-        'title':'Data Analyst',
-        'location':'Delhi,India',
-        'salary':'Rs. 15,000'
-    },
-        {
-        'id':1,
-        'title':'Backend Developer',
-        'location':'Delhi,India',
-        'salary':'Rs. 15,000'
-    },
-        {
-        'id':1,
-        'title':'frontend developer',
-        'location':'Delhi,India',
-        'salary':'Rs. 18,000'
-    },
-    {
-        'id': 1,
-        'title': 'Data scientist',
-        'location': 'Delhi,India',
-        'salary': 'Rs. 17,000'
-    }
-]
+
 @app.route('/')
 def home():
-    return render_template('home.html',jobs=JOBS)
+    jobs = load_jobs()
+    return render_template('home.html',jobs=jobs)
 @app.route('/api/jobs')
 def jobs():
-    return jsonify(JOBS)
+    return jsonify(jobs)
+
+@app.route('/job/<id>')
+def showjob(id:int):
+    job=load_job(id)
+    if not job:
+        return 'Not Found',404
+    else:
+        requirements=job['requirements'].split('.')
+        responsibilities = job['responsibilities'].split('.')
+        return render_template('singlejob.html', job=job,requirements=requirements,responsibilities=responsibilities)
+
+
+@app.route('/job/<id>/apply', methods=['POST'])
+def applyjob(id):
+    application = request.form
+    apply_job(application,id)
+    job=load_job(id)
+    return render_template('applicationsubmitted.html',job=job,application=job)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
